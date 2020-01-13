acdd0a8d4c6f7838401bf1f0bff0abb0f42b1286
峰 <kbuddy@users.noreply.github.com>
2020-01-13 11:07:37 +0800

    *

--- generate.sh
+++ generate.sh
@@ -3,8 +3,9 @@
 set -e
 
 curl -L -o GeoLite2-Country-CSV.zip "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country-CSV&license_key=${LICENSE_KEY}&suffix=zip"
-zip -FF GeoLite2-Country-CSV.zip --out GeoLite2-Country-CSV2.zip | unzip
+zip -FF GeoLite2-Country-CSV.zip --out GeoLite2-Country-CSV2.zip
 rm GeoLite2-Country-CSV.zip
+unzip GeoLite2-Country-CSV2.zip
 rm GeoLite2-Country-CSV2.zip
 mv GeoLite2* geoip
 ls ./geoip
