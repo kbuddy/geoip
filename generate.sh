dbf47f264f69aab27159d87f67dc0c127a71c60c
峰 <kbuddy@users.noreply.github.com>
2020-01-13 11:32:55 +0800

    *

---
+++ generate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+curl -L -o GeoLite2-Country-CSV.zip "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country-CSV&license_key=${LICENSE_KEY}&suffix=zip"
+ls -l GeoLite2-Country-CSV.zip
+unzip GeoLite2-Country-CSV.zip
+rm GeoLite2-Country-CSV.zip
+mv GeoLite2* geoip
+ls ./geoip
+
+go get -u github.com/v2ray/geoip
+$GOPATH/bin/geoip --country=./geoip/GeoLite2-Country-Locations-en.csv --ipv4=./geoip/GeoLite2-Country-Blocks-IPv4.csv --ipv6=./geoip/GeoLite2-Country-Blocks-IPv6.csv
+
+mkdir ./publish
+mv ./geoip.dat ./publish/
